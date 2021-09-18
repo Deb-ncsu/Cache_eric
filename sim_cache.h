0ec8ce12536bc487c6ede2604cf611e82d4e8e29
Deb-ncsu <dadak@ncsu.edu>
2021-09-18 09:57:52 +0530

    commit	new file:   Makefile commit	new file:   gcc.debug0.txt commit	new file:   sim_cache commit	new file:   sim_cache.cc commit	new file:   sim_cache.h commit	new file:   trace2.txt

---
+++ sim_cache.h
@@ -0,0 +1,16 @@
+#include "cache.h"
+#ifndef SIM_CACHE_H
+#define SIM_CACHE_H
+
+typedef struct cache_params{
+    unsigned long int block_size;
+    unsigned long int l1_size;
+    unsigned long int l1_assoc;
+    unsigned long int vc_num_blocks;
+    unsigned long int l2_size;
+    unsigned long int l2_assoc;
+}cache_params;
+
+// Put additional data structures here as per your requirement
+
+#endif
